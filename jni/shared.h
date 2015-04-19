b06696a5711aa6be9da7077431deb91b4586fa97
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2015-04-18 23:26:26 -0500

    Nuther simple fix

--- jni/shared.h
+++ jni/shared.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <android/log.h>
-#include "mcpe.h"
+#include "developer/mcpe.h"
 
 #define LOG_TAG "AtomicLauncher"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
